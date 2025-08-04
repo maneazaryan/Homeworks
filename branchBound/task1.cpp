d6559fb0d3ce61bb9be06efea7d6735495216a30
ManeAza <azaryanmane@gmail.com>
2025-08-04 22:25:43 +0400

    Did some changes in task1.cpp

--- branchBound/task1.cpp
+++ branchBound/task1.cpp
@@ -10,9 +10,9 @@ struct bestCase{
 	bestCase() : v(0), sum(0){}
 };
 void bestCase::print(){
-	for(int a:v) {
-		if(a!=v[v.size()-1])std::cout << a << '+';
-		else std::cout<< a << '='<< sum <<std::endl;
+	for(int a=0; a< v.size(); a++) {
+		if(a!=v.size()-1)std::cout << v[a] << '+';
+		else std::cout<< v[a] << '='<< sum <<std::endl;
 	}
 }
 void checkSum(std::vector<int>& v, int n, int max, int a)
@@ -44,12 +44,13 @@ void reverseV(std::vector<int>&v, int size, int max)
 	{
 		rV[i]=v[size-1-i];
 	}
+	//for(int a: rV) std::cout<< a<< ' ';
 	checkSum(rV, size, max ,1);
 }
 int main()
 {
 	std::vector<int> v= {3,5,6,7,8};
-	int maxSum=1;
+	int maxSum=12;
 	checkSum(v, v.size(), maxSum ,0);
 	reverseV(v, v.size(),maxSum);
 	return 0;
